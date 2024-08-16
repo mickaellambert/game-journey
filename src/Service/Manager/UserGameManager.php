bbbdc637bcab1feb8d79e9d27b7da85db63d5e05
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-08-16 03:40:17 +0200

    Fix for add a game to collection

--- src/Service/Manager/UserGameManager.php
+++ src/Service/Manager/UserGameManager.php
@@ -2,17 +2,20 @@
 
 namespace App\Service\Manager;
 
-use App\Exception\GameNotFoundException;
-use App\Exception\InvalidGameDataException;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Entity\Game;
+use App\Entity\User;
 use App\DTO\UserGameDTO;
+use App\Entity\Platform;
 use App\Entity\UserGame;
+use App\Exception\GameAlreadyInCollectionException;
 use App\Repository\GameRepository;
+use App\Repository\UserRepository;
+use App\Exception\NotFoundException;
 use App\Repository\PlatformRepository;
 use App\Repository\UserGameRepository;
-use App\Repository\UserRepository;
+use App\Exception\InvalidDataException;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserGameManager
 {
@@ -22,71 +25,51 @@ class UserGameManager
         private GameRepository $gameRepository,
         private UserRepository $userRepository,
         private PlatformRepository $platformRepository,
-        private GameManager $gameManager,
         private ValidatorInterface $validator
         )
     {}
 
-    public function add(UserGameDTO $dto): array
+    public function add(UserGameDTO $dto): UserGame
     {
         $errors = $this->validator->validate($dto);
         
         if (count($errors) > 0) {
-            $errorMessages = [];
-            
-            foreach ($errors as $error) {
-                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
-            }
-
-            return [
-                'status' => 'error',
-                'code' => Response::HTTP_BAD_REQUEST,
-                'errors' => $errorMessages
-            ];
+            throw new InvalidDataException($errors[0]->getMessage());
         }
 
-        $user = $this->userRepository->find($dto->user_id);
+        $user = $this->userRepository->find($dto->getUserId());
         
         if (!$user) {
-            return [
-                'status' => 'error',
-                'code' => Response::HTTP_NOT_FOUND,
-                'errors' => ['user' => 'User not found']
-            ];
+            throw new NotFoundException(User::class);
         }
 
-        $game = $this->gameManager->findOrCreate((array) $dto);
+        $game = $this->gameRepository->find($dto->getGameId());
 
         if (!$game) {
-            return [
-                'status' => 'error',
-                'code' => Response::HTTP_NOT_FOUND,
-                'errors' => ['user' => 'User not found']
-            ];
+            throw new NotFoundException(Game::class);
+        }
+
+        $platform = $this->platformRepository->find($dto->getPlatformId());
+        
+        if (!$platform || !$game->getPlatforms()->contains($platform)) {
+            throw new NotFoundException(Platform::class);
         }
 
         $userGame = $this->userGameRepository->findOneBy(['user' => $user, 'game' => $game]);
 
         if ($userGame) {
-            return [
-                'status' => 'error',
-                'code' => Response::HTTP_CONFLICT,
-                'errors' => ['game' => 'User already has this game in their collection']
-            ];
+            throw new GameAlreadyInCollectionException();
         }
 
         $userGame = new UserGame();
         $userGame->setUser($user);
         $userGame->setGame($game);
-        $userGame->setPlatform($this->platformRepository->findOrCreate(['name' => $dto->platform]));
-        $userGame->setStatus($dto->status);
+        $userGame->setPlatform($platform);
+        $userGame->setStatus($dto->getStatus());
 
         $this->entityManager->persist($userGame);
         $this->entityManager->flush();
 
-        return [
-            'status' => 'success',
-            'code' => Response::HTTP_CREATED,
-        ];
+        return $userGame;
     }
 }
